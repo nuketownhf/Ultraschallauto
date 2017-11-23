8130526a78b93a9cc03b3860051bd77df6c28ca0
nuketownhf <nuketownhf@gmail.com>
2017-11-23 19:56:44 +0100

    Kollisionsdetection

--- ultraschallauto.py
+++ ultraschallauto.py
@@ -4,25 +4,45 @@ import time
 
 
 class Ultraschallauto():
-    def __init__(self, root):
+    def __init__(self):
         super().__init__(master = None)
         
 
     def draw(self):
-    
+        global carpic, car, cvs, move
         root = Tk()
         root.title("Distanzmesser")
         root.resizable(width=False, height=False)
         cvs = Canvas(bg='white', width = 1400, height = 300)
         cvs.pack()
-        global carpic, Ultraschallauto
+        
         carpic = PhotoImage(file="D:\\GitHub\\Ultraschallauto\\car.png")
         car = cvs.create_image(500, 160, image=carpic, anchor=NW)
+        
         wand = cvs.create_rectangle(0, 0, 50, 320, fill="grey")
         #--
-        global ok
-        ok = cvs.bind("<1>", lambda event: cvs.move(car, -2,0))
+        cvs.bind("<1>", lambda event: Ultraschallauto.drive(1))
+
+
+
+
+    def drive(self):
+        global cvs, move, car
+        cvs.move(car, -2,0)
+        coords = cvs.coords(car)
         
+
+        if coords == [21.0, 160.0]:
+            print("AUTO KOMPLETT ZERSTÖRT LO WO HAST DU FAHREN GELERNT MORRUK")
+            print("NEUSTART in...")
+            time.sleep(1)
+            print("3...")
+            time.sleep(1)
+            print("2...")
+            time.sleep(1)
+            print("1...")
+            time.sleep(1)
+            cvs.coords(car, 500,160)
         
         
         
