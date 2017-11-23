e1e21770317c8a83ce7e5caa2470722831467c34
nuketownhf <nuketownhf@gmail.com>
2017-11-23 17:58:58 +0100

    Bild importiert

--- ultraschallauto.py
+++ ultraschallauto.py
@@ -1,19 +1,29 @@
 from tkinter import *
+from PIL import Image, ImageTk
 import time
 
 
-class Ultraschallauto:
+class Ultraschallauto():
     def __init__(self, auto, wand, abstand):
-        super().__init__()
-        self.auto = auto
-        self.wand = wand
-        self.abstand = abstand
+        super().__init__(master = None)
+        
+
+    def draw(self):
+    
+        root = Tk()
+        cvs = Canvas(bg='black', width = 900, height = 800)
+        cvs.pack()
+        global carpic
+        carpic = PhotoImage(file="D:\\GitHub\\Ultraschallauto\\car.png")
+        car = cvs.create_image(5, 5, image=carpic, anchor=NW)
+        
+        
+        
+        
+        
+        
 
 #----------------------------------
 
-main = Tk()
-main.title("hi")
-main.geometry("800x500")
-main.mainloop()
-
+Ultraschallauto.draw(1)
 
