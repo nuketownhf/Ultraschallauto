2b9069f28fe66ce3cb799b709d06bcdd5cd00e15
nuketownhf <nuketownhf@gmail.com>
2017-11-23 21:10:46 +0100

    Kommentierung + Bugfix

--- ultraschallauto.py
+++ ultraschallauto.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import time
+import os
+
 
 
 class Ultraschallauto():
@@ -7,37 +9,33 @@ class Ultraschallauto():
         super().__init__(master = None)
         
 
-    def draw(self):
+    def draw(self): #canvas design
         global carpic, car, cvs, move, wand
-        root = Tk()
-        root.title("Distanzmesser")
-        root.resizable(width=False, height=False)
-        cvs = Canvas(bg='white', width = 1400, height = 300)
-        
+        root = Tk() 
+        root.title("Distanzmesser") # Titel des Programmes
+        root.resizable(width=False, height=False) # Fenstergröße nicht veränderbar
+        cvs = Canvas(bg='white', width = 1400, height = 300) #Canvas Fläche
         cvs.pack()
-        
-        carpic = PhotoImage(file="D:\\GitHub\\Ultraschallauto\\car.png")
-        car = cvs.create_image(1000, 160, image=carpic, anchor=NW)
-        
-        wand = cvs.create_rectangle(0, 0, 50, 320, fill="grey")
-        #--
-        cvs.bind("<1>", lambda event: Ultraschallauto.drive(1))
+        carpic = PhotoImage(file=abs_file_path) #Funktion zum Importieren des Bildes
+        car = cvs.create_image(1000, 160, image=carpic, anchor=NW)# Importiert das Bild
+        wand = cvs.create_rectangle(0, 0, 50, 320, fill="grey")# Importiert die Wand
+        cvs.bind("<1>", lambda event: Ultraschallauto.fahren(1)) #Mausklick Event
 
 
 
 
-    def drive(self):
+    def fahren(self): # 
         global cvs, move, car, coords, wand
-        cvs.move(car, -5,0)
+        cvs.move(car, -5,0) # verschiebt das Auto nach links
         coords = cvs.coords(car)
             
-        if coords == [700.0, 160.0]:
+        if coords == [700.0, 160.0]: #verändert die Farbe der Wand in Green
             wand = cvs.create_rectangle(0, 0, 50, 320, fill="green")
 
-        elif coords == [350.0, 160.0]:
+        elif coords == [350.0, 160.0]:#verändert die Farbe der Wand in Gelb
             wand = cvs.create_rectangle(0, 0, 50, 320, fill="yellow")
 
-        elif coords == [100.0, 160.0]:
+        elif coords == [100.0, 160.0]:#verändert die Farbe der Wand in Rot
             wand = cvs.create_rectangle(0, 0, 50, 320, fill="red")
     
         elif coords <= [15.0, 160.0]:
@@ -57,5 +55,7 @@ class Ultraschallauto():
 
 #----------------------------------
 
+script_dir = os.path.dirname(__file__)
+rel_path = "car.png"
+abs_file_path = os.path.join(script_dir, rel_path)
 Ultraschallauto.draw(1)
-print("test")
